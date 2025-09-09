aeb12bfbe7df68832235c9033d0ac7883298f4ef
Gaurav Kaushik <gauravkaushik1710@gmail.com>
2025-09-10 02:02:59 +0530

    Q1: Fetch Users API React App

---
+++ q1_fetch_users/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Users List</h2>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>{u.name} - {u.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
